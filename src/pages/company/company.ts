8d53ab23ca1922684da2f9ffe6ed5358b054542a
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-06-06 21:42:24 -0400

    added company listview

--- src/pages/company/company.ts
+++ src/pages/company/company.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AboutPage } from './../about/about';
+import { Component, ViewChild } from '@angular/core';
+import {List, IonicPage,  NavController,  NavParams} from 'ionic-angular';
 
 /**
  * Generated class for the CompanyPage page.
@@ -13,12 +14,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'company.html',
 })
 export class CompanyPage {
+  public companyList: Array<string>;
+  @ViewChild(List) list: List;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  ionViewDidEnter(){
+     this.companyList = ["a", "b", "c"]; //JSON.parse(localStorage.getItem("companies"));
+     if(!this.companyList) {
+            this.companyList = [];
+        }
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CompanyPage');
   }
 
+  edit(index: number){
+    this.companyList.splice(index, 1);
+    localStorage.setItem("company", JSON.stringify(this.companyList));
+  }
+
+  add(){
+    this.navCtrl.push(AboutPage);
+  }
 }
