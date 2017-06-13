bb9d51e64b69b3e419b00a6bd4277db42bf6f0c2
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-06-13 16:30:13 -0400

    function edit implemented to inpection

--- src/pages/inspection-add/inspection-add.ts
+++ src/pages/inspection-add/inspection-add.ts
@@ -16,13 +16,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class InspectionAddPage {
   public inspectionList: Array<InspectionModel>;
   public inspectionItem: InspectionModel;
+  public title: string;
+  public edit: boolean;
+  public index: number;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.edit = false;
+
     this.inspectionList = JSON.parse(localStorage.getItem("inspection"));
     if(!this.inspectionList) {
       this.inspectionList = [];
     }
-    this.inspectionItem = new InspectionModel;
+
+    this.index = navParams.get("index");
+    if(this.index != undefined) {
+      this.title = "Editar";
+      this.inspectionItem = this.inspectionList[this.index];
+      this.edit = true;
+    }else{
+      this.title = "Adicionar";
+      this.inspectionItem = new InspectionModel;
+    }
   }
 
   ionViewDidLoad() {
@@ -30,8 +44,15 @@ export class InspectionAddPage {
   }
 
   save(){
+
+
     if (this.inspectionItem.description){
-      this.inspectionList.push(this.inspectionItem);
+      if(this.edit){
+        this.inspectionItem.updated_at = Date.now();
+        this.inspectionList[this.index] = this.inspectionItem;
+      }else{
+        this.inspectionList.push(this.inspectionItem);
+      }
       localStorage.setItem("inspection", JSON.stringify(this.inspectionList));
       this.navCtrl.pop();
     }
