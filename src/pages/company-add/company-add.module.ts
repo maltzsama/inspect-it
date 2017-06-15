fc1bfa1e294e92df7ef5aec62e1843520725ac30
Kaue Pereira <kauefontes@outlook.com>
2017-06-14 22:07:13 -0400

    Change the type of cnpj and phone fields to call the correct keyboard on phone

--- src/pages/company-add/company-add.module.ts
+++ src/pages/company-add/company-add.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CompanyAddPage } from './company-add';
+import { AppMaskerModule } from 'brmasker-ionic';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { CompanyAddPage } from './company-add';
   ],
   imports: [
     IonicPageModule.forChild(CompanyAddPage),
+    AppMaskerModule
   ],
   exports: [
     CompanyAddPage
