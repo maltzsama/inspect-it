5f68e22525687cdbf41acdb250a168ed26a4cdaa
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-06-21 17:14:43 -0400

    now ok button is hidden when we have to show information about occurrence

--- src/pages/occurrence-add/occurrence-add.ts
+++ src/pages/occurrence-add/occurrence-add.ts
@@ -23,9 +23,10 @@ export class OccurrenceAddPage {
   public occurrenceList: Array<OccurrenceModel>;
   public occurrenceItem: OccurrenceModel;
   public title: string;
-  public edit: boolean;
+  public saveOrEdit: boolean;
   public index: number;
   public inspection_id: string;
+  public edit: boolean;
   
   public placeholder = 'assets/img/placeholder.png';
   public chosenPicture: any;
@@ -33,6 +34,7 @@ export class OccurrenceAddPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public actionsheetCtrl: ActionSheetController,
     public cameraProvider: CameraProvider, public platform: Platform, public loadingCtrl: LoadingController) {
+    this.saveOrEdit = false;
     this.edit = false;
 
     this.occurrenceList = JSON.parse(localStorage.getItem("occurrence"));
@@ -42,10 +44,13 @@ export class OccurrenceAddPage {
 
     this.index = navParams.get("index");
     this.inspection_id = navParams.get("inspection_id");
+    this.edit = navParams.get("edit");
+    
+    console.log(this.edit);
     if(this.index != undefined) {
       this.title = "Editar";
       this.occurrenceItem = this.occurrenceList[this.index];
-      this.edit = true;
+      this.saveOrEdit = true;
       if(this.occurrenceItem.photo){
         this.chosenPicture = this.occurrenceItem.photo;
         this.placeholder = this.occurrenceItem.photo;
@@ -54,11 +59,14 @@ export class OccurrenceAddPage {
       this.title = "Adicionar";
       this.occurrenceItem = new OccurrenceModel;
     }
+    if(!this.edit){
+      this.title = "";
+    }
   }
 
   save(){
     if (this.occurrenceItem.description){
-      if(this.edit){
+      if(this.saveOrEdit){
         this.occurrenceItem.updated_at = Date.now();
         if(this.chosenPicture){
           this.occurrenceItem.photo = this.chosenPicture;
