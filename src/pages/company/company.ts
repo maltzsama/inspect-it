d4420c8311f3d28a726033d0d91caac3b2efe733
Demetrius Albuquerque <demetrius.albuquerque@yahoo.com.br>
2017-06-26 21:00:24 -0400

    added function to filter companys by name

--- src/pages/company/company.ts
+++ src/pages/company/company.ts
@@ -18,16 +18,22 @@ import {List, IonicPage,  NavController,  NavParams} from 'ionic-angular';
 })
 export class CompanyPage {
   public companyList: Array<CompanyModel>;
+  public filterCompany:string;
   @ViewChild(List) list: List;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidEnter(){
+    this.fullCompany();
+  }
+
+  private fullCompany(){
     this.companyList = JSON.parse(localStorage.getItem("company"));
     if(!this.companyList) {
       this.companyList = [];
     }
+    return this.companyList;
   }
 
   ionViewDidLoad() {
@@ -56,5 +62,15 @@ export class CompanyPage {
     this.navCtrl.push(InspectionPage, {
       company: this.companyList[idx]
     });
+  }
+
+  onInput(event){
+    this.fullCompany();
+    let val = event.target.value;
+    if (val && val.trim() != '') {
+      this.companyList = this.companyList.filter((company) => {
+        return (company.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      });
+    }
   }
 }
